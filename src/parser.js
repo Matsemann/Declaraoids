b43f5c98afce84c87031c3d8d694469813bec0bd
Mats Krüger Svensson <mats@matsemann.com>
2016-10-23 12:52:42 +0200

    Implement the beginning of the parser, passing the tests

--- src/parser.js
+++ src/parser.js
@@ -1,2 +1,16 @@
-module.exports = function () {
+module.exports = function (query) {
+    var findQuery = query.substring(4);
+    var find = findQuery.split('And').map(convertBackToCamelCase);
+
+    return {
+        find
+    }
 };
+
+function convertBackToCamelCase(string) {
+    return string.split('_').map(lowerCaseFirst).join('_');
+}
+
+function lowerCaseFirst(string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
+}
