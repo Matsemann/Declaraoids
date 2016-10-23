56adf38d83fc557f9ab7e998ea797c28d21241d4
Mats Krüger Svensson <mats@matsemann.com>
2016-10-23 13:07:23 +0200

    Implement the where-part. Refactored into separate methods after everything worked

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,53 @@
+var comparisons = ["NotEquals", "Equals", "LessThan", "GreaterThan", "Includes"]
+
 module.exports = function (query) {
-    var findQuery = query.substring(4);
-    var find = findQuery.split('And').map(convertBackToCamelCase);
+
+    var {findQuery, whereQuery} = splitQuery(query);
+
+    var find = generateFind(findQuery);
+    var where = generateWhere(whereQuery);
 
     return {
-        find
+        find,
+        where
     }
 };
+
+function splitQuery(query) {
+    var parts = query.split('Where');
+
+    var findQuery = parts[0].substring(4);
+    var whereQuery = parts[1];
+
+    return {
+        findQuery,
+        whereQuery
+    };
+}
+
+function generateFind(findQuery) {
+    return findQuery.split('And').map(convertBackToCamelCase);
+}
+
+function generateWhere(whereQuery) {
+    if (!whereQuery) {
+        return [];
+    }
+
+    var parts = whereQuery.split('And');
+
+    return parts.map(part => {
+        var comparison = comparisons.find(c => part.includes(c));
+        var split = part.split(comparison);
+
+        return {
+            property: convertBackToCamelCase(split[0]),
+            comparison: comparison.toLowerCase(),
+            input: lowerCaseFirst(split[1])
+        };
+    });
+
+}
 
 function convertBackToCamelCase(string) {
     return string.split('_').map(lowerCaseFirst).join('_');
