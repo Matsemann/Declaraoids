7dc7e36d1ef0dccd3b02e33e6b5ba34b314b2353
Mats Krüger Svensson <mats@matsemann.com>
2016-10-23 13:43:27 +0200

    Tests for the finder's find stuff. Started on the implementation, can extract the values but not nested values

--- src/finder.js
+++ src/finder.js
@@ -1,13 +1,29 @@
+var parser = require('./parser');
+
 module.exports = (array) => {
     return new Proxy(array, {
         get (target, property) { return finder(target, property); }
     });
 };
 
-function finder(array, property) {
-    if (!property.startsWith('find')) {
-        return array[property];
+function finder(array, query) {
+    if (!query.startsWith('find')) {
+        return array[query];
     }
 
-    return property;
+    var parsed = parser(query);
+
+
+    var mapFunc = e => {
+        var obj = {};
+        parsed.find.forEach(find => {
+            obj[find] = e[find];
+        });
+        return obj;
+    };
+
+
+    return () => {
+        return array.map(mapFunc);
+    }
 }
--- test/finder.spec.js
+++ test/finder.spec.js
@@ -7,15 +7,59 @@ var p3 = { name: "Linn", age: 22, sex: "F", address: { city: "Bergen"} };
 
 
 describe('Finder', () => {
-    var impl = finder([p1, p2, p3]);
+    var array2 = finder([p1, p2]);
+    var array3 = finder([p1, p2, p3]);
 
     it('calls underlying array when not starting with find', () => {
-        assert.deepEqual(p3, impl[2]);
-        assert.equal(3, impl.length);
+        assert.deepEqual(p3, array3[2]);
+        assert.equal(3, array3.length);
     });
 
-    it('echoes back stuff starting with find', () => {
-        assert.equal('findName', impl.findName);
-        assert.equal('findWaldo', impl.findWaldo);
+    describe('find variables', () => {
+
+        it('should find a single variable, name', () => {
+            var result = array3.findName();
+
+            var expected = [{ name: "Mats"}, { name: "Kåre"}, {name: "Linn"}];
+
+            assert.deepEqual(result, expected)
+        });
+
+        it('should find a multiple variables, name and age', () => {
+            var result = array2.findNameAndAge();
+            var expected = [{
+                name: "Mats",
+                age: 25
+            }, {
+                name: "Kåre",
+                age: 31
+            }];
+            assert.deepEqual(result, expected)
+        });
+        it('should find nested variables', () => {
+            var result = array2.findNameAndAddress_City();
+            var expected = [{
+                name: "Mats",
+                address_city: "Oslo"
+            }, {
+                name: "Kåre",
+                address_city: "Bergen"
+            }];
+            assert.deepEqual(result, expected)
+        });
+        it('should find supernested variables', () => {
+            var input = {
+                levelOne: {
+                    levelTwo: {
+                        levelThree: "hey"
+                    }
+                }
+            };
+            var result = finder([input]).findLevelOne_LevelTwo_LevelThree();
+            var expected = [{
+                levelOne_levelTwo_levelThree: "hey"
+            }];
+            assert.deepEqual(result, expected)
+        });
     });
 });
