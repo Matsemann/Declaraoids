f73acda06abccd4bca092ead48acfc16cf4d089e
Mats Krüger Svensson <mats@matsemann.com>
2016-10-23 12:51:47 +0200

    Adding tests for how the parser should extract what is queried for

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,7 +1,25 @@
 var assert = require('assert');
 var parser = require('../src/parser.js');
 
+// Query should be on the form findNameAndAgeWhereAddress_CityEqualsCity
 
 describe('Parser', () => {
-
+    describe('find clause', () => {
+        it('finds a single variable', () => {
+            var expected = ['name'];
+            assert.deepEqual(parser('findName').find, expected);
+        });
+        it('finds multiple variables', () => {
+            var expected = ['name', 'address'];
+            assert.deepEqual(parser('findNameAndAddress').find, expected);
+        });
+        it('assumes camelCase', () => {
+            var expected = ['name', 'streetAddress'];
+            assert.deepEqual(parser('findNameAndStreetAddress').find, expected);
+        });
+        it('finds nested properties and camelCases them', () => {
+            var expected = ['name', 'address_houseNumber'];
+            assert.deepEqual(parser('findNameAndAddress_HouseNumber').find, expected);
+        });
+    });
 });
